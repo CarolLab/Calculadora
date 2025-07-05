c4de223227ce7cc315e83d00c44da12d953e19c1
CarolLab <bessascarolina@gmail.com>
2025-07-05 18:19:19 +0100

    Cor do monitor alterada, código melhorado

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 #Janela
 
-#Por noutra versão no github
+
 # def apagar_uma():
 #     monitor["text"] -= monitor["text"][:-1]
 
@@ -41,24 +41,54 @@ def resultado(tela): #Reseltado
 
 
 ################################################################
-################################################################
+
+#Janela
+
 janela = Tk()
 
 janela.geometry("234x330")
 janela.title("Calculadora")
 #Adicionar icon
-#Adicionar cor nos simbolos
 #Mudar a fonte da letras/ números
 
 
-#Botões######################################
-
-monitor = Label(janela, text="", anchor= "e", relief = "flat",justify= RIGHT,padx = 7, font = "Ivy 16",
-                width = 18, height= 2,
-                bg="Light Grey", fg="Black")
+#Monitor
+monitor = Label(janela, text="", anchor= "e", relief = "flat",justify= RIGHT,
+                width = 18, height= 2, padx = 7,
+                bg="#423f3f", fg="White",font = "Ivy 16")
 monitor.place(x = 0,y = 3)
 
 
+
+#Operadores
+#Divisão
+divid = Button(janela, text="/", width=7, height = 3, bg = "Orange", relief="raised",
+               command= lambda: mostrar("/"))
+divid.place(x = 175, y = 58)
+
+
+#Multiplicação
+mult = Button(janela, text = "*", width=7, height=3, bg = "Orange",relief="raised",
+              command = lambda: mostrar("*"))
+mult.place(x = 175, y = 113)
+
+
+#Subtração
+subtrair = Button(janela, text = "-", width=7,height=3, bg = "Orange",relief="raised",
+                  command=lambda: mostrar("-"))
+subtrair.place(x = 175, y = 168)
+
+
+#Adição
+somar = Button(janela, text= "+", width=7,height=3,bg = "Orange",relief= "raised",
+               command= lambda: mostrar("+"))
+somar.place(x = 175, y = 223)
+
+
+
+
+
+#Apagar
 apagtud = Button(janela, text="C", width=15, height = 3, padx=2,
                  command= apagar2) #tamanho original 14
 apagtud.place(x = 0, y = 58)
@@ -68,88 +98,79 @@ apagar = Button(janela, width = 7, height = 3, text = "⌫",anchor="center",stat
 apagar.place(x = 116, y = 58)
 
 
-#Por noutra versão no github
-# apag1 = Button(janela, text = "Apagar",width=8, command=apagar_uma)
-# apag1.grid(row = 1, column = 5)
-#Fazer com que seja flequesivel, quando aumentar os botões também e a mesma coisa quando diminui
-#Não deixar diminiuir abaixo do padrão (234x330)
 
 #A função lambda cria uma função anónima sem nome, que executa a função mostrar() quando o botão seja clicado
 #def funcao():
 #    mostrar("/")
 
-divid = Button(janela, text="/", width=7, height = 3, bg = "Orange",
-               command= lambda: mostrar("/"))
-divid.place(x = 175, y = 58)
-
-sete = Button(janela, text= "7", width=7,height=3, relief= "raised",
+#Linha 1
+sete = Button(janela, text= "7", width=7,height=3,
+              relief= "raised", overrelief="flat",
               command= lambda: mostrar("7"))
 sete.place(x = 0, y= 113)
 
-oito = Button(janela, text="8",width=7,height=3, relief= "raised",
+oito = Button(janela, text="8",width=7,height=3,
+              relief= "raised", overrelief= "flat",
               command= lambda: mostrar("8"))
 oito.place(x = 58, y = 113)
 
-nove = Button(janela, text = "9",width=7,height=3, relief= "raised",
+nove = Button(janela, text = "9",width=7,height=3,
+              relief= "raised",overrelief="flat",
               command = lambda: mostrar("9"))
 nove.place(x = 116, y = 113)
 
 
-mult = Button(janela, text = "*", width=7, height=3, bg = "Orange",
-              command = lambda: mostrar("*"))
-mult.place(x = 175, y = 113)
 
-
-
-quatro = Button(janela, text = "4",width=7,height=3, relief= "raised",
+#Linha2
+quatro = Button(janela, text = "4",width=7,height=3,
+                relief= "raised", overrelief="flat",
                 command = lambda: mostrar("4"))
 quatro.place(x = 0, y = 168)
 
-cinco = Button(janela, text = "5", width=7,height=3, relief= "raised",
+cinco = Button(janela, text = "5", width=7,height=3,
+               relief= "raised", overrelief="flat",
                command= lambda: mostrar("5"))
 cinco.place(x = 58, y = 168)
 
-seis= Button(janela, text = "6",width=7,height=3, relief= "raised",
+seis= Button(janela, text = "6",width=7,height=3,
+             relief= "raised", overrelief="flat",
              command= lambda: mostrar("6"))
 seis.place(x = 116, y = 168)
 
-subtrair = Button(janela, text = "-", width=7,height=3, bg = "Orange",
-                  command=lambda: mostrar("-"))
-subtrair.place(x = 175, y = 168)
 
 
-
-
-um = Button(janela, text = "1", width=7,height=3, relief= "raised",
+#Linha3
+um = Button(janela, text = "1", width=7,height=3,
+            relief= "raised", overrelief="flat",
             command= lambda: mostrar("1"))
 um.place(x=0, y =223)
 
-dois = Button(janela, text = "2", width=7,height=3, relief= "raised",
+dois = Button(janela, text = "2", width=7,height=3,
+              relief= "raised", overrelief="flat",
               command= lambda: mostrar("2"))
 dois.place(x = 58, y = 223)
 
-tres = Button(janela, text="3", width=7,height=3, relief= "raised",
+tres = Button(janela, text="3", width=7,height=3,
+              relief= "raised", overrelief="flat",
               command= lambda: mostrar("3"))
 tres.place(x = 116, y = 223)
 
-somar = Button(janela, text= "+", width=7,height=3,bg = "Orange",
-               command= lambda: mostrar("+"))
-somar.place(x = 175, y = 223)
 
 
-
-
-zero = Button(janela, text = "0", width=15, height = 3,padx=2, relief= "raised",
+#Linha4
+zero = Button(janela, text = "0", width=15, height = 3,padx=2,
+              relief= "raised", overrelief="flat",
               command= lambda: mostrar("0"))
 zero.place(x = 0, y = 278)
 
 ponto = Button(janela, text = ".", width=7,height=3,
+               relief="raised", overrelief="flat",
                command= lambda: mostrar("."))
 ponto.place(x = 116, y = 278)
 
-igual = Button(janela, text= "=", width=7,height=3,bg= "Orange",
+igual = Button(janela, text= "=", width=7,height=3,bg= "Orange", relief= "raised",
                command=lambda : resultado(monitor))
 igual.place(x = 175, y = 278)
 
-
+###################
 janela.mainloop()
